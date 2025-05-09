e47af08e3fcbbb5269adaa2d73e94f2cf3958937
Sri Hema Saranya Sekar <saranya.s.umashankar@gmail.com>
2025-05-08 19:05:48 -0700

    removed the parameter

--- src/test/java/tests/RecipeScrapper.java
+++ src/test/java/tests/RecipeScrapper.java
@@ -1,4 +1,4 @@
-package Tests;
+package tests;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,17 +20,16 @@ import scrappers.FilterLowCarbRecipe;
 import scrappers.FilterLowFatRecipe;
 import scrappers.ScrappedDatas;
 
-
 public class RecipeScrapper {
 
-@Test(priority = 1)
+	@Test(priority = 1)
 
-	public static void recipeDataInfo(Integer categoryId, String url) throws IOException, InterruptedException {
+	public static void recipeDataInfo() throws IOException, InterruptedException {
 
 		BrowserFactory bf = new BrowserFactory();
 		bf.browsersetup("chrome");
 		WebDriver driver = BrowserFactory.getdriverinstance();
-		ScrappedDatas SD=new ScrappedDatas();
+		ScrappedDatas SD = new ScrappedDatas();
 		FilterLowFatRecipe lfdiet = new FilterLowFatRecipe();
 		FilterLowCarbRecipe lcdiet = new FilterLowCarbRecipe();
 		FilterAllergytables allergy = new FilterAllergytables();
@@ -41,34 +40,36 @@ public class RecipeScrapper {
 		RecipesScrapper recipesScrapper = new RecipesScrapper();
 
 		Map<Integer, RecipeUrlInfo> recipeMap = recipesScrapper.scrapeRecipeCategories();
-		for (Map.Entry<Integer, RecipeUrlInfo> entry : recipeMap.entrySet()) 
-		{
-			RecipeUrlInfo url_Info=entry.getValue();
-			String value =url_Info.getUrl();
-			String recipiemethod=SD.recipie_method(value, driver);
-			String recipiedescription=SD.recipie_description(value, driver);
-			String recipie_nutrient_values=SD.recipie_nutrient_values(value, driver);
+		for (Map.Entry<Integer, RecipeUrlInfo> entry : recipeMap.entrySet()) {
+			RecipeUrlInfo url_Info = entry.getValue();
+			String value = url_Info.getUrl();
+			String recipiemethod = SD.recipie_method(value, driver);
+			String recipiedescription = SD.recipie_description(value, driver);
+			String recipie_nutrient_values = SD.recipie_nutrient_values(value, driver);
 			List<String> liststringingredients = scdata.getIngredients();
 			String ingredients = String.join(", ", liststringingredients).toLowerCase();
 
-	
-			 String LFtablename = lfdiet.recipecheckLowFatdiet(ingredients, recipiemethod);
-			 LoggerLoad.info("TABLE NAME: "+LFtablename);
-			//Make a call to the appropriate class.method() to connect to postgresql and insert into above said Low fat table
-			 
-			 String LCtablename = lcdiet.recipecheckLowCarbdiet(ingredients, recipiemethod);
-			 LoggerLoad.info("TABLE NAME: "+LCtablename);
-			//Make a call to the appropriate class.method() to connect to postgresql and insert into above said Low carbs table
-			 
-			 ArrayList<String> Allergytables= allergy.allergylist(ingredients);
-			 if(Allergytables.size()>0) {
-				 for(String allergytable:Allergytables) {
-					 LoggerLoad.info("TABLE NAME: "+allergytable);
-					 //Make a call to the appropriate class.method() to connect to postgresql and insert into above said allergy table
-				 										}
-			//CsvWriterUtil.writeRow(categoryId, info.getUrl(), recipeId, info.getRecipeName(), info.getUrl());
+			String LFtablename = lfdiet.recipecheckLowFatdiet(ingredients, recipiemethod);
+			LoggerLoad.info("TABLE NAME: " + LFtablename);
+			// Make a call to the appropriate class.method() to connect to postgresql and
+			// insert into above said Low fat table
+
+			String LCtablename = lcdiet.recipecheckLowCarbdiet(ingredients, recipiemethod);
+			LoggerLoad.info("TABLE NAME: " + LCtablename);
+			// Make a call to the appropriate class.method() to connect to postgresql and
+			// insert into above said Low carbs table
+
+			ArrayList<String> Allergytables = allergy.allergylist(ingredients);
+			if (Allergytables.size() > 0) {
+				for (String allergytable : Allergytables) {
+					LoggerLoad.info("TABLE NAME: " + allergytable);
+					// Make a call to the appropriate class.method() to connect to postgresql and
+					// insert into above said allergy table
+				}
+				// CsvWriterUtil.writeRow(categoryId, info.getUrl(), recipeId,
+				// info.getRecipeName(), info.getUrl());
+			}
+
 		}
-		
 	}
-}
 }
